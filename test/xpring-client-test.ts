c2494cb3135db5542909392ba354a83465c19793
Keefer Taylor <keefer.t@gmail.com>
2019-08-13 18:13:24 -0400

    Install protoc during CI

--- test/xpring-client-test.ts
+++ test/xpring-client-test.ts
@@ -1,10 +1,10 @@
 import { assert } from "chai";
-import XpringClient from "../src/xpring-client"
+import XpringClient from "../src/xpring-client";
 import "mocha";
 
 describe("Xpring Client", function() {
-  /** 
-   * Placeholder test to bootstrap project. 
+  /**
+   * Placeholder test to bootstrap project.
    * TODO(keefertaylor): Delete this test.
    */
   it("Client can be constructed", function() {
@@ -12,4 +12,3 @@ describe("Xpring Client", function() {
     assert.exists(client);
   });
 });
-
