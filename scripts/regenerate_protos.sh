63ab1b77cbcfa3b10af7d903d6bebd68939316a6
Keefer Taylor <keefer.t@gmail.com>
2019-09-13 14:56:40 -0700

    Update

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -6,17 +6,22 @@ echo "Regenerating Protocol Buffers"
 
 mkdir -p ./generated
 
-# Path to this plugin
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
-
 # Directory to write generated code to (.js and .d.ts files)
 OUT_DIR="./generated"
 
+# Generate node code.
+grpc_tools_node_protoc \
+    --js_out=import_style=commonjs,binary:$OUT_DIR \
+    --grpc_out=.$OUT_DIR \
+    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+     --proto_path=$PWD/terram-protos/proto \
+     $PWD/terram-protos/**/*.proto
+ 
+# Generate tyepscript declaration files. 
 protoc \
-    --proto_path=$PWD/terram-protos/proto \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
-    --ts_out="service=true:${OUT_DIR}" \
-    $PWD/terram-protos/**/*.proto
+    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+    --ts_out=$OUT_DIR \
+     --proto_path=$PWD/terram-protos/proto \
+     $PWD/terram-protos/**/*.proto
 
 echo "All done!"
