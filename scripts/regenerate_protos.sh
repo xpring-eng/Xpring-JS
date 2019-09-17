7d841f5e63bd0fcf90720e6c6e9cb91f8d068885
Keefer Taylor <keefer.t@gmail.com>
2019-09-16 17:03:35 -0700

    Fix script

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -12,16 +12,16 @@ OUT_DIR="./generated"
 # Generate node code.
 grpc_tools_node_protoc \
     --js_out=import_style=commonjs,binary:$OUT_DIR \
-    --grpc_out=.$OUT_DIR \
+    --grpc_out=$OUT_DIR \
     --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
      --proto_path=$PWD/terram-protos/proto \
      $PWD/terram-protos/**/*.proto
- 
-# Generate tyepscript declaration files. 
+
+# Generate tyepscript declaration files.
 protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=$OUT_DIR \
-     --proto_path=$PWD/terram-protos/proto \
-     $PWD/terram-protos/**/*.proto
+    --proto_path=$PWD/terram-protos/proto \
+    $PWD/terram-protos/**/*.proto
 
 echo "All done!"
