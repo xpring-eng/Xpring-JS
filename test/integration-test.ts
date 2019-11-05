06a5eaf063ddb69aa3e2b2e83ebabfcfc6b0e39a
Keefer Taylor <keefer.t@gmail.com>
2019-11-04 16:54:15 -0800

    re-enable integration tests

--- test/integration-test.ts
+++ test/integration-test.ts
@@ -1,39 +1,39 @@
-// import { Wallet, XRPAmount } from "xpring-common-js";
-// import XpringClient from "../src/xpring-client";
-// import { assert } from "chai";
+import { Wallet, XRPAmount } from "xpring-common-js";
+import XpringClient from "../src/xpring-client";
+import { assert } from "chai";
 
-// /* eslint-disable @typescript-eslint/no-non-null-assertion */
-// /* eslint-disable @typescript-eslint/no-magic-numbers */
-// /* global BigInt */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* global BigInt */
 
-// // A timeout for these tests.
-// const timeoutMs = 60 * 1000; // 1 minute
+// A timeout for these tests.
+const timeoutMs = 60 * 1000; // 1 minute
 
-// // An address on TestNet that has a balance.
-// const recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4";
+// An address on TestNet that has a balance.
+const recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4";
 
-// // A wallet with some balance on TestNet.
-// const wallet = Wallet.generateWalletFromSeed("snYP7oArxKepd3GPDcrjMsJYiJeJB")!;
+// A wallet with some balance on TestNet.
+const wallet = Wallet.generateWalletFromSeed("snYP7oArxKepd3GPDcrjMsJYiJeJB")!;
 
-// // The XpringClient that makes requests
-// const grpcURL = "grpc.xpring.tech:80";
-// const xrpClient = XpringClient.xpringClientWithEndpoint(grpcURL);
+// The XpringClient that makes requests
+const grpcURL = "grpc.xpring.tech:80";
+const xrpClient = XpringClient.xpringClientWithEndpoint(grpcURL);
 
-// // Some amount of XRP to send.
-// const amount = BigInt("1");
+// Some amount of XRP to send.
+const amount = BigInt("1");
 
-// describe("Xpring JS Integration Tests", function(): void {
-//   it("Get Account Balance", async function() {
-//     this.timeout(timeoutMs);
+describe("Xpring JS Integration Tests", function(): void {
+  it("Get Account Balance", async function() {
+    this.timeout(timeoutMs);
 
-//     const balance = await xrpClient.getBalance(recipientAddress);
-//     assert.exists(balance);
-//   });
+    const balance = await xrpClient.getBalance(recipientAddress);
+    assert.exists(balance);
+  });
 
-//   it("Send XRP", async function() {
-//     this.timeout(timeoutMs);
+  it("Send XRP", async function() {
+    this.timeout(timeoutMs);
 
-//     const result = await xrpClient.send(amount, recipientAddress, wallet);
-//     assert.exists(result);
-//   });
-// });
+    const result = await xrpClient.send(amount, recipientAddress, wallet);
+    assert.exists(result);
+  });
+});
