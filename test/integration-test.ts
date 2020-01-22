e39ba1985d96f175e6baef8dcde35bc59676f4fb
Keefer Taylor <keefer.t@gmail.com>
2020-01-22 12:16:24 -0500

    Update integration-test.ts

--- test/integration-test.ts
+++ test/integration-test.ts
@@ -1,71 +1,53 @@
 import { Wallet } from "xpring-common-js";
-import { XpringClientDecorator } from "./xpring-client-decorator";
-import LegacyDefaultXpringClient from "./legacy/legacy-default-xpring-client";
-import TransactionStatus from "./transaction-status";
-import ReliableSubmissionXpringClient from "./reliable-submission-xpring-client";
+import XpringClient from "../src/xpring-client";
+import { assert } from "chai";
+import TransactionStatus from "../src/transaction-status";
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 /* global BigInt */
 
-/**
- * XpringClient is a client which interacts with the Xpring platform.
- */
-class XpringClient {
-  private readonly decoratedClient: XpringClientDecorator;
+// A timeout for these tests.
+const timeoutMs = 60 * 1000; // 1 minute
 
-  /**
-   * Create a new XpringClient.
-   *
-   * The XpringClient will use gRPC to communicate with the given endpoint.
-   *
-   * @param grpcURL The URL of the gRPC instance to connect to.
-   */
-  public constructor(grpcURL: string) {
-    const defaultXpringClient = LegacyDefaultXpringClient.defaultXpringClientWithEndpoint(
-      grpcURL
+// An address on TestNet that has a balance.
+const recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4";
+
+// A wallet with some balance on TestNet.
+const wallet = Wallet.generateWalletFromSeed("snYP7oArxKepd3GPDcrjMsJYiJeJB")!;
+
+// A hash of a successfully validated transaction.
+const transactionHash =
+  "9A88C8548E03958FD97AF44AE5A8668896D195A70CF3FF3CB8E57096AA717135";
+
+// The XpringClient that makes requests
+const grpcURL = "grpc.xpring.tech:80";
+const xpringClient = new XpringClient(grpcURL);
+
+// Some amount of XRP to send.
+const amount = BigInt("1");
+
+describe("Xpring JS Integration Tests", function(): void {
+  it("Get Account Balance", async function() {
+    this.timeout(timeoutMs);
+
+    const balance = await xpringClient.getBalance(recipientAddress);
+    assert.exists(balance);
+  });
+
+  it("Get Transaction Status", async function() {
+    this.timeout(timeoutMs);
+
+    const transactionStatus = await xpringClient.getTransactionStatus(
+      transactionHash
     );
+    assert.deepEqual(transactionStatus, TransactionStatus.Succeeded);
+  });
 
-    this.decoratedClient = new ReliableSubmissionXpringClient(
-      defaultXpringClient
-    );
-  }
+  it("Send XRP", async function() {
+    this.timeout(timeoutMs);
 
-  /**
-   * Retrieve the balance for the given address.
-   *
-   * @param address The X-Address to retrieve a balance for.
-   * @returns A `BigInt` representing the number of drops of XRP in the account.
-   */
-  public async getBalance(address: string): Promise<BigInt> {
-    return await this.decoratedClient.getBalance(address);
-  }
-
-  /**
-   * Retrieve the transaction status for a given transaction hash.
-   *
-   * @param transactionHash The hash of the transaction.
-   * @returns The status of the given transaction.
-   */
-  public async getTransactionStatus(
-    transactionHash: string
-  ): Promise<TransactionStatus> {
-    return await this.decoratedClient.getTransactionStatus(transactionHash);
-  }
-
-  /**
-   * Send the given amount of XRP from the source wallet to the destination address.
-   *
-   * @param drops A `BigInt`, number or numeric string representing the number of drops to send.
-   * @param destination A destination address to send the drops to.
-   * @param sender The wallet that XRP will be sent from and which will sign the request.
-   * @returns A promise which resolves to a string representing the hash of the submitted transaction.
-   */
-  public async send(
-    amount: BigInt | number | string,
-    destination: string,
-    sender: Wallet
-  ): Promise<string> {
-    return await this.decoratedClient.send(amount, destination, sender);
-  }
-}
-
-export default XpringClient;
+    const result = await xpringClient.send(amount, recipientAddress, wallet);
+    assert.exists(result);
+  });
+});
