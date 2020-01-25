7ed20dbec328b367205b897d5b43f6d8f2747310
Tyler Storm <stormtv@mail.uc.edu>
2020-01-24 21:24:15 -0500

    don't remove these for now

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export {
   AccountInfo,
+  ClassicAddress,
   SubmitSignedTransactionResponse,
   XRPAmount,
   Wallet,
+  WalletGenerationResult,
   Utils,
 } from 'xpring-common-js'
 export { default as XpringClient } from './xpring-client'
