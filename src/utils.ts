c7bb9c58ecd0482528a43b2cf2b263fa1b63ac84
Tyler Storm <stormtv@mail.uc.edu>
2020-01-30 14:54:45 -0500

    way better

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,5 @@
 const isNode = (): boolean => {
-  return (
-    typeof process !== 'undefined' &&
-    typeof process.release !== 'undefined' &&
-    process.release.name === 'node'
-  )
+  return process?.release?.name === 'node'
 }
 
 export default isNode
