a366d28ab2aee2b764ee1ad90495fe76f60c0656
Amie Corso <acorso@ripple.com>
2020-03-09 16:40:32 -0700

    almost there with xrp-issued-currency

---
+++ src/xrp-issued-currency.ts
@@ -0,0 +1,29 @@
+// import bigInt from 'big-integer'
+import bigInt from 'bigint-typescript-definitions'
+import XRPCurrency from './xrp-currency'
+import { IssuedCurrencyAmount } from './generated/web/org/xrpl/rpc/v1/amount_pb'
+
+/*
+ * An issued currency on the XRP Ledger
+ * - SeeAlso: https://xrpl.org/basic-data-types.html#specifying-currency-amounts
+ */
+export default class XRPIssuedCurrency {
+  public currency: XRPCurrency | undefined
+
+  public value: BigInteger | undefined
+
+  public issuer: string | undefined
+
+  public constructor(issuedCurrency: IssuedCurrencyAmount) {
+    const value = bigInt(issuedCurrency.getValue())
+    if (!value) {
+      return
+    }
+    this.value = value
+    const currency = issuedCurrency.getCurrency()
+    if (currency) {
+      this.currency = new XRPCurrency(currency)
+    }
+    this.issuer = issuedCurrency.getIssuer()?.getAddress()
+  }
+}
