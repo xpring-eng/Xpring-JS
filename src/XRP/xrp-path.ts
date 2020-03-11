8179d54dfc6e5ee56be0ad1c10f4bdba27e32814
Amie Corso <acorso@ripple.com>
2020-03-11 13:34:01 -0700

    XRPPath constructor needs to call static factory for XRPPathElements too

--- src/XRP/xrp-path.ts
+++ src/XRP/xrp-path.ts
@@ -9,7 +9,7 @@ type PathElement = Payment.PathElement
  * - SeeAlso: https://xrpl.org/paths.html
  */
 export default class XRPPath {
-  public pathElements: XRPPathElement[]
+  public pathElements: (XRPPathElement | undefined)[]
 
   public static from(path: Path): XRPPath | undefined {
     return new XRPPath(path)
@@ -17,8 +17,8 @@ export default class XRPPath {
 
   private constructor(path: Path) {
     const protoPathElements: PathElement[] = path.getElementsList()
-    this.pathElements = protoPathElements.map(
-      (pathElement) => new XRPPathElement(pathElement),
+    this.pathElements = protoPathElements.map((pathElement) =>
+      XRPPathElement.from(pathElement),
     )
   }
 }
