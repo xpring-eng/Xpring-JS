ab13da9f06c5d0ed57026f42096881787de1efeb
Amie Corso <acorso@ripple.com>
2020-03-11 13:00:09 -0700

    XRPCurrency and tests

---
+++ src/XRP/xrp-currency.ts
@@ -0,0 +1,16 @@
+import { Currency } from '../generated/web/org/xrpl/rpc/v1/amount_pb'
+
+/**
+ * An issued currency on the XRP LEdger
+ * - SeeAlso: https://xrpl.org/currency-formats.html#currency-codes
+ */
+export default class XRPCurrency {
+  public name: string
+
+  public code: string | Uint8Array
+
+  public constructor(currency: Currency) {
+    this.name = currency.getName()
+    this.code = currency.getCode()
+  }
+}
