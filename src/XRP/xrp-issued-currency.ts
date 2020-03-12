19acddf60a16d83682c2b32651daf44c28fb4baa
Amie Corso <acorso@ripple.com>
2020-03-12 07:57:51 -0700

    @see and refactor w/ parameter properties

--- src/XRP/xrp-issued-currency.ts
+++ src/XRP/xrp-issued-currency.ts
@@ -4,18 +4,17 @@ import { IssuedCurrencyAmount } from '../generated/web/org/xrpl/rpc/v1/amount_pb
 
 /*
  * An issued currency on the XRP Ledger
- * - SeeAlso: https://xrpl.org/basic-data-types.html#specifying-currency-amounts
+ * @see: https://xrpl.org/basic-data-types.html#specifying-currency-amounts
  */
 export default class XRPIssuedCurrency {
-  public currency: XRPCurrency | undefined
-
-  public value: BigInteger | undefined
-
-  public issuer: string | undefined
-
   public static from(
     issuedCurrency: IssuedCurrencyAmount,
   ): XRPIssuedCurrency | undefined {
+    const currency = issuedCurrency.getCurrency()
+    let xrpCurrency
+    if (currency) {
+      xrpCurrency = XRPCurrency.from(currency)
+    }
     let value
     try {
       value = bigInt(issuedCurrency.getValue())
@@ -23,17 +22,15 @@ export default class XRPIssuedCurrency {
       value = undefined
     }
     if (value) {
-      return new XRPIssuedCurrency(issuedCurrency)
+      const issuer = issuedCurrency.getIssuer()?.getAddress()
+      return new XRPIssuedCurrency(xrpCurrency, value, issuer)
     }
     return undefined
   }
 
-  private constructor(issuedCurrency: IssuedCurrencyAmount) {
-    this.value = bigInt(issuedCurrency.getValue())
-    const currency = issuedCurrency.getCurrency()
-    if (currency) {
-      this.currency = XRPCurrency.from(currency)
-    }
-    this.issuer = issuedCurrency.getIssuer()?.getAddress()
-  }
+  private constructor(
+    readonly currency?: XRPCurrency,
+    readonly value?: BigInteger,
+    readonly issuer?: string,
+  ) {}
 }
