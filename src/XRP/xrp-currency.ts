6bd173a61dfdf5178f29dc1318c39f370a7dc8fc
Amie Corso <acorso@ripple.com>
2020-03-12 07:28:38 -0700

    @see and refactor w/ parameter properties

--- src/XRP/xrp-currency.ts
+++ src/XRP/xrp-currency.ts
@@ -2,19 +2,15 @@ import { Currency } from '../generated/web/org/xrpl/rpc/v1/amount_pb'
 
 /**
  * An issued currency on the XRP LEdger
- * - SeeAlso: https://xrpl.org/currency-formats.html#currency-codes
+ * @see: https://xrpl.org/currency-formats.html#currency-codes
  */
 export default class XRPCurrency {
-  public name: string
-
-  public code: string | Uint8Array
-
   public static from(currency: Currency): XRPCurrency | undefined {
-    return new XRPCurrency(currency)
+    return new XRPCurrency(currency.getName(), currency.getCode())
   }
 
-  private constructor(currency: Currency) {
-    this.name = currency.getName()
-    this.code = currency.getCode()
-  }
+  private constructor(
+    readonly name: string,
+    readonly code: string | Uint8Array,
+  ) {}
 }
