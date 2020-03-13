532ea155199eb65547ec409011d8cb9bf490022f
Keefer Taylor <keefer.t@gmail.com>
2020-03-13 21:13:46 +0300

    working tests

--- src/PayID/pay-id-client.ts
+++ src/PayID/pay-id-client.ts
@@ -1,5 +1,10 @@
-import axios, { AxiosInstance } from 'axios'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/require-await */
+// import axios, { AxiosInstance } from 'axios'
 import { PayIDUtils } from 'xpring-common-js'
+import DefaultApi from './api/DefaultApi'
+import ApiClient from './ApiClient'
 import PayIDError, { PayIDErrorType } from './pay-id-error'
 
 /**
@@ -10,11 +15,11 @@ import PayIDError, { PayIDErrorType } from './pay-id-error'
  */
 export default class PayIDClient {
   /** An HTTP client. */
-  private readonly axiosInstance: AxiosInstance
+  // private readonly axiosInstance: AxiosInstance
 
-  public constructor() {
-    this.axiosInstance = axios.create({})
-  }
+  // public constructor() {
+  //   // this.axiosInstance = axios.create({})
+  // }
 
   /**
    * Retrieve the XRP Address authorized with a PayID.
@@ -30,47 +35,72 @@ export default class PayIDClient {
       throw PayIDError.invalidPaymentPointer
     }
 
-    const url = `https://${paymentPointer.host}${paymentPointer.path}`
-    // TODO(keefertaylor): Attach network parameter.
-    // TODO(keefertaylor): Generalize the below to allow requesting in other types.
-    const requestConfig = {
-      headers: {
-        Accept: 'application/xrp+json',
-      },
-    }
+    const client = new ApiClient()
+    client.basePath = `https://${paymentPointer.host}`
+    const apiInstance = new DefaultApi(client)
 
-    let response
-    try {
-      response = await this.axiosInstance.get<{ address: string }>(
-        url,
-        requestConfig,
-      )
-    } catch (error) {
-      // Handle erroneous responses from the server.
-      if (error.response) {
-        // 404 means no mapping was found.
-        if (error.response.status === 404) {
-          return undefined
+    // Swagger generates the '/' in the URL by default and the payment pointer's 'path' is prefixed by a '/'. Strip off the leading '/'.
+    const userAndHost = paymentPointer.path.substring(1)
+
+    return new Promise((resolve, reject) => {
+      apiInstance.getUserAndHost(userAndHost, (error, data, _response) => {
+        if (error) {
+          if (error.status === 404) {
+            // Not Found
+            resolve(undefined)
+          } else {
+            const message = `${error.status}: ${error.response.text}`
+            reject(new PayIDError(PayIDErrorType.UnexpectedResponse, message))
+          }
+        } else if (data?.address) {
+          resolve(data.address)
+        } else {
+          reject(new PayIDError(PayIDErrorType.UnexpectedResponse))
         }
+      })
+    })
 
-        // Otherwise re-throw the error as an unexepected response.
-        throw new PayIDError(
-          PayIDErrorType.UnexpectedResponse,
-          `${error.response.status} ${error.response.statusText}: ${error.response.data}`,
-        )
-      } else {
-        // Generically errors from the server which contained no response (timeouts, etc).
-        throw new PayIDError(PayIDErrorType.Unknown, error.message)
-      }
-    }
+    // const url = `https://${paymentPointer.host}${paymentPointer.path}`
+    // // TODO(keefertaylor): Attach network parameter.
+    // // TODO(keefertaylor): Generalize the below to allow requesting in other types.
+    // const requestConfig = {
+    //   headers: {
+    //     Accept: 'application/xrp+json',
+    //   },
+    // }
 
-    const address = response.data?.address
-    if (!address) {
-      throw new PayIDError(
-        PayIDErrorType.UnexpectedResponse,
-        'Sucessful response was in an unknown format',
-      )
-    }
-    return address
+    // let response
+    // try {
+    //   response = await this.axiosInstance.get<{ address: string }>(
+    //     url,
+    //     requestConfig,
+    //   )
+    // } catch (error) {
+    //   // Handle erroneous responses from the server.
+    //   if (error.response) {
+    //     // 404 means no mapping was found.
+    //     if (error.response.status === 404) {
+    //       return undefined
+    //     }
+
+    //     // Otherwise re-throw the error as an unexepected response.
+    //     throw new PayIDError(
+    //       PayIDErrorType.UnexpectedResponse,
+    //       `${error.response.status} ${error.response.statusText}: ${error.response.data}`,
+    //     )
+    //   } else {
+    //     // Generically errors from the server which contained no response (timeouts, etc).
+    //     throw new PayIDError(PayIDErrorType.Unknown, error.message)
+    //   }
+    // }
+
+    // const address = response.data?.address
+    // if (!address) {
+    //   throw new PayIDError(
+    //     PayIDErrorType.UnexpectedResponse,
+    //     'Sucessful response was in an unknown format',
+    //   )
+    // }
+    // return address
   }
 }
