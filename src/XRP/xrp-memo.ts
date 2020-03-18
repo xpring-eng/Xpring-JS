85513b5984d3febe185360c15280f0dd625fa08b
Amie Corso <acorso@ripple.com>
2020-03-18 08:28:58 -0700

    getValue() --> getValue_asU8() and types updated to not be polymorphic

--- src/XRP/xrp-memo.ts
+++ src/XRP/xrp-memo.ts
@@ -6,15 +6,15 @@ import { Memo } from '../generated/web/org/xrpl/rpc/v1/transaction_pb'
  */
 export default class XRPMemo {
   public static from(memo: Memo): XRPMemo | undefined {
-    const data = memo.getMemoData()?.getValue()
-    const format = memo.getMemoFormat()?.getValue()
-    const type = memo.getMemoType()?.getValue()
+    const data = memo.getMemoData()?.getValue_asU8()
+    const format = memo.getMemoFormat()?.getValue_asU8()
+    const type = memo.getMemoType()?.getValue_asU8()
     return new XRPMemo(data, format, type)
   }
 
   private constructor(
-    readonly data?: Uint8Array | string,
-    readonly format?: Uint8Array | string,
-    readonly type?: Uint8Array | string,
+    readonly data?: Uint8Array,
+    readonly format?: Uint8Array,
+    readonly type?: Uint8Array,
   ) {}
 }
