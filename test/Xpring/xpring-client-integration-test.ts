c00c49fc53e28de939088c0bd0f50afa7aa1e465
Keefer Taylor <keefer.t@gmail.com>
2020-03-18 20:32:57 +0300

    integration tests

---
+++ test/Xpring/xpring-client-integration-test.ts
@@ -0,0 +1,39 @@
+import { assert } from 'chai'
+import { Wallet } from 'xpring-common-js'
+import XpringClient from '../../src/Xpring/xpring-client'
+import PayIDClient, { XRPLNetwork } from '../../src/PayID/pay-id-client'
+import XRPClient from '../../src/xrp-client'
+
+// A timeout for these tests.
+const timeoutMs = 60 * 1000 // 1 minute
+
+// The network to conduct tests on.
+const network = XRPLNetwork.Test
+
+// A wallet with some balance on TestNet.
+const wallet = Wallet.generateWalletFromSeed('snYP7oArxKepd3GPDcrjMsJYiJeJB')!
+
+// A PayIDClient under test.
+const payIDClient = new PayIDClient()
+
+// An XRPClient under test.
+const rippledURL = 'test.xrp.xpring.io:50051'
+const xrpClient = new XRPClient(rippledURL, true)
+
+// A XpringClient under test.
+const xpringClient = new XpringClient(network, payIDClient, xrpClient)
+
+describe('Xpring Integration Tests', function(): void {
+  it('Send XRP TestNet', async function(): Promise<void> {
+    this.timeout(timeoutMs)
+
+    // GIVEN a Pay ID that will resolve.
+    const payID = '$dev.payid.xpring.money/hbergren'
+
+    // WHEN XRP is sent to the Pay ID.
+    const transactionHash = await xpringClient.send(10, payID, wallet)
+
+    // THEN a transaction hash is returned.
+    assert.exists(transactionHash)
+  })
+})
