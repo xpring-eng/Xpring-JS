7bb8bf6a4e48a471391ed911b01ab4f9430c0568
Amie <acorso@ripple.com>
2020-03-19 07:34:04 -0700

    (3/n) Path / PathElement (#249)
    * path and pathelement classes
    
    * XRPCurrency and tests
    
    * refactor for static factory constructor
    
    * conversion tests
    
    * make constructor private
    
    * replace new XRPCurrency(...) with XRPCurrency.from(...)
    
    * refactor to use static factory constructors, private constructors
    
    * refactor tests to use the static factory constructors
    
    * XRPPath constructor needs to call static factory for XRPPathElements too
    
    * update tests with optional chainging to make compiler happy
    
    * optional chaining in tests
    
    * @see and refactor w/ parameter properties
    
    * @see and refactor w/ parameter properties
    
    * update XRPCurrency class w @see and parameter properties
    
    * add ? to currency object in tests
    
    * remove MARK comment
    
    * drop undefined return type from XRPCurrency.from
    
    * don't need ? on currency in tests now
    
    * drop undefined from initializers
    
    * get code as bytes, document XRPCurrency fields
    
    * update test comments
    
    * use && instead of if/else

---
+++ src/XRP/xrp-path-element.ts
@@ -0,0 +1,24 @@
+import { Payment } from '../generated/web/org/xrpl/rpc/v1/transaction_pb'
+import XRPCurrency from './xrp-currency'
+
+type PathElement = Payment.PathElement
+
+/*
+ * A path step in an XRP Ledger Path.
+ * @see: https://xrpl.org/paths.html#path-steps
+ */
+export default class XRPPathElement {
+  public static from(pathElement: PathElement): XRPPathElement {
+    const account = pathElement.getAccount()?.getAddress()
+    const currency = pathElement.getCurrency()
+    const xrpCurrency = currency && XRPCurrency.from(currency)
+    const issuer = pathElement.getIssuer()?.getAddress()
+    return new XRPPathElement(account, xrpCurrency, issuer)
+  }
+
+  private constructor(
+    readonly account?: string,
+    readonly currency?: XRPCurrency,
+    readonly issuer?: string,
+  ) {}
+}
