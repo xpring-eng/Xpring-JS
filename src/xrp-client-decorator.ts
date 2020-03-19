d0d8832da3ef16e83415a32f8f6785ebe754131b
Amie Corso <acorso@ripple.com>
2020-03-19 14:26:21 -0700

    paymentHistory imp in reliable-submission client

--- src/xrp-client-decorator.ts
+++ src/xrp-client-decorator.ts
@@ -2,6 +2,7 @@ import { Wallet } from 'xpring-common-js'
 import { BigInteger } from 'big-integer'
 import TransactionStatus from './transaction-status'
 import RawTransactionStatus from './raw-transaction-status'
+import XRPTransaction from './XRP/xrp-transaction'
 
 /** A decorator interface for XRPClients. */
 export interface XRPClientDecorator {
@@ -63,4 +64,15 @@ export interface XRPClientDecorator {
    * @returns A boolean if the account is on the blockchain.
    */
   accountExists(address: string): Promise<boolean>
+
+  /**
+   * Return the history of payments for the given account.
+   * Note: This method only works for payment type transactions, see: https://xrpl.org/payment.html
+   * Note: This method only returns the history that is contained on the remote node, which may not contain a full history of the network.
+   *
+   * @param address: The address (account) for which to retrive payment history.
+   * @throws: An error if there was a problem communicating with the XRP Ledger.
+   * @return: An array of transactions associated with the account.
+   */
+  paymentHistory(address: string): Promise<Array<XRPTransaction>>
 }
