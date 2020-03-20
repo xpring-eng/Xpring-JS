20c9030cdb6beac28ee7cbe118ce3baf87a171cf
Keefer Taylor <keefer.t@gmail.com>
2020-03-20 13:47:38 +0300

    restore build script

--- scripts/regenerate_pay_id_api.sh
+++ scripts/regenerate_pay_id_api.sh
@@ -1,60 +1,60 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# set -e -o pipefail
+set -e -o pipefail
 
-# # Folder to place the generated classes in.
-# DESTINATION_FOLDER=./src/PayID/generated/
+# Folder to place the generated classes in.
+DESTINATION_FOLDER=./src/PayID/generated/
 
-# # Open API Specfication to use for generation.
-# PAY_ID_OPEN_API_SPEC=./pay-id-api-spec/pay-id.v1.yml
+# Open API Specfication to use for generation.
+PAY_ID_OPEN_API_SPEC=./pay-id-api-spec/pay-id.v1.yml
 
-# # Temporary directory to write files to.
-# TMP_SWAGGER_DIR=./.tmp_swagger
+# Temporary directory to write files to.
+TMP_SWAGGER_DIR=./.tmp_swagger
 
-# # Language to generate.
-# LANG=javascript
+# Language to generate.
+LANG=javascript
 
-# # Folder containing generated sources.
-# GENERATED_SOURCES_FOLDER=$TMP_SWAGGER_DIR/src
+# Folder containing generated sources.
+GENERATED_SOURCES_FOLDER=$TMP_SWAGGER_DIR/src
 
-# ##########################################################################
-# # Remove any stale files which are already generated.
-# ##########################################################################
-# echo "Removing stale files"
-# rm -rf $DESTINATION_FOLDER
-# echo "Done removing stale files"
+##########################################################################
+# Remove any stale files which are already generated.
+##########################################################################
+echo "Removing stale files"
+rm -rf $DESTINATION_FOLDER
+echo "Done removing stale files"
 
-# ##########################################################################
-# # Regenerate Swagger API
-# ##########################################################################
+##########################################################################
+# Regenerate Swagger API
+##########################################################################
 
-# echo "Regenerating Swagger API from PayID Open API Spec"
+echo "Regenerating Swagger API from PayID Open API Spec"
 
-# mkdir -p $TMP_SWAGGER_DIR
-# mkdir -p $DESTINATION_FOLDER
+mkdir -p $TMP_SWAGGER_DIR
+mkdir -p $DESTINATION_FOLDER
 
-# java -jar swagger-codegen.jar generate \
-#   --lang $LANG \
-#   --input $PAY_ID_OPEN_API_SPEC \
-#   -o $TMP_SWAGGER_DIR \
+java -jar swagger-codegen.jar generate \
+  --lang $LANG \
+  --input $PAY_ID_OPEN_API_SPEC \
+  -o $TMP_SWAGGER_DIR \
 
-# echo "Swagger Generation Complete!"
+echo "Swagger Generation Complete!"
 
-# ##########################################################################
-# # Copy artifacts and remove excess files
-# #
-# # Swagger outputs a lot of helper boilerplate by default which we don't need.
-# # Copy the source files to the directory and then delete the remainder.
-# #
-# # TODO(keefertaylor): There's probably a better way to do this. Investigate.
-# ##########################################################################
+##########################################################################
+# Copy artifacts and remove excess files
+#
+# Swagger outputs a lot of helper boilerplate by default which we don't need.
+# Copy the source files to the directory and then delete the remainder.
+#
+# TODO(keefertaylor): There's probably a better way to do this. Investigate.
+##########################################################################
 
-# echo "Cleaning Up...."
+echo "Cleaning Up...."
 
-# # Copy Source files
-# cp -r $GENERATED_SOURCES_FOLDER/* $DESTINATION_FOLDER
-# # Remove everythinge else.
-# # rm -rf $TMP_SWAGGER_DIR
+# Copy Source files
+cp -r $GENERATED_SOURCES_FOLDER/* $DESTINATION_FOLDER
+# Remove everythinge else.
+rm -rf $TMP_SWAGGER_DIR
 
-# echo "Done Cleaning Up"
-# echo "All Done!"
+echo "Done Cleaning Up"
+echo "All Done!"
