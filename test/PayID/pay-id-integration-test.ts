780c46f1b388753a6b1d7678c54469fcb770b2de
Keefer Taylor <keefer.t@gmail.com>
2020-03-20 20:51:30 +0300

    fix tests

--- test/PayID/pay-id-integration-test.ts
+++ test/PayID/pay-id-integration-test.ts
@@ -78,6 +78,8 @@ describe('PayID Integration Tests', function(): void {
   })
 
   it('receipt', async function(): Promise<void> {
+    // GIVEN a Pay ID.
+    const payID = '$travel.payid.xpring.money/dino'
     const payIDClient = new PayIDClient(XRPLNetwork.Main)
 
     // WHEN the Pay ID receipt endpoint is hit then an error is not thrown.
