0e80c67ae7d7fcea5e3fb90fad0028b064affa1c
Amie Corso <acorso@ripple.com>
2020-03-26 08:29:47 -0700

    fake grpc error

---
+++ test/XRP/fakes/fake-grpc-error.ts
@@ -0,0 +1,19 @@
+import grpc from 'grpc'
+
+/**
+ * Convenience class for creating more specific Error objects to mimick gRPC error responses.
+ */
+export default class FakeGRPCError extends Error {
+  /**
+   * Construct a new FakeServiceError.
+   *
+   * @param message The text details of the error.
+   * @param code The grpc.status code.
+   */
+  constructor(
+    public readonly message: string,
+    public readonly code: grpc.status,
+  ) {
+    super(message)
+  }
+}
