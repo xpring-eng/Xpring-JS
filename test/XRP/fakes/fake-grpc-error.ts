131250789ef33e5252f3f2d33d15e96edd683ada
Amie Corso <acorso@ripple.com>
2020-03-30 10:36:35 -0700

    TODO around fake-grpc merge

--- test/XRP/fakes/fake-grpc-error.ts
+++ test/XRP/fakes/fake-grpc-error.ts
@@ -3,6 +3,7 @@ import grpc from 'grpc'
 /**
  * Convenience class for creating more specific Error objects to mimick gRPC error responses.
  */
+// TODO(amiecorso): merge xrp and ilp fake-grpc-errors into common location
 export default class FakeGRPCError extends Error {
   /**
    * Construct a new FakeServiceError.
