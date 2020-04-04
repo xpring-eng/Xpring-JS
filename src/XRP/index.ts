373fd9478b490aa534b094722d1204414d032607
Amie Corso <acorso@ripple.com>
2020-04-04 07:48:11 -0700

    reorganize top level index

--- src/XRP/index.ts
+++ src/XRP/index.ts
@@ -1,3 +1,6 @@
+export { default as RippledFlags } from './rippled-flags'
+export { default as TransactionStatus } from './transaction-status'
+export { default as XRPClient } from './xrp-client'
 export {
   XRPCurrencyAmount,
   XRPCurrency,
