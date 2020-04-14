eeb5a65babd88944405158ec059438a7168b7cd6
Amie Corso <acorso@ripple.com>
2020-04-14 15:04:59 -0700

    update comment on XRPPayment.destinationTag

--- src/XRP/model/xrp-payment.ts
+++ src/XRP/model/xrp-payment.ts
@@ -72,7 +72,7 @@ export default class XRPPayment {
    *
    * @param amount The amount of currency to deliver.
    * @param destination The unique address of the account receiving the payment.
-   * @param destinationTag (Optional) Arbitrary tag that identifies the reason for the payment.
+   * @param destinationTag (Optional) Arbitrary tag that identifies a hosted recipient to pay, or the reason for the payment.
    * @param deliverMin (Optional) Minimum amount of destination currency this transaction should deliver.
    * @param invoiceID (Optional) Arbitrary 256-bit hash representing a specific reason or identifier for this payment.
    * @param paths Array of payment paths to be used for this transaction.  Must be omitted for XRP-to-XRP transactions.
