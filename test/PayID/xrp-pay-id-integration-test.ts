88d50e069dcc9a4dce40f0013e11724989babae0
Keefer Taylor <keefer.t@gmail.com>
2020-04-22 02:14:30 +0900

    fix tests

--- test/PayID/xrp-pay-id-integration-test.ts
+++ test/PayID/xrp-pay-id-integration-test.ts
@@ -7,15 +7,15 @@ import ComplianceType from '../../src/PayID/compliance-type'
 // A timeout for these tests.
 const timeoutMs = 60 * 1000 // 1 minute
 
-describe('PayID Integration Tests', function(): void {
-  it('Resolve PayID to XRP - known PayID - mainnet', async function(): Promise<
+describe('PayID Integration Tests', function (): void {
+  it('Resolve PayID to XRP - known PayID - mainnet', async function (): Promise<
     void
   > {
     this.timeout(timeoutMs)
 
     // GIVEN a Pay ID that will resolve on Mainnet.
     const payIDClient = new XRPPayIDClient(XRPLNetwork.Main)
-    const payID = '$pay.michael.zochow.ski'
+    const payID = '$dev.payid.xpring.money/alice'
 
     // WHEN it is resolved to an XRP address
     const xrpAddress = await payIDClient.addressForPayID(payID)
@@ -24,14 +24,14 @@ describe('PayID Integration Tests', function(): void {
     assert.equal(xrpAddress, 'X7zmKiqEhMznSXgj9cirEnD5sWo3iZSHqcZEd67ddMfJG9Y')
   })
 
-  it('Resolve PayID to XRP - known PayID - testnet', async function(): Promise<
+  it('Resolve PayID to XRP - known PayID - testnet', async function (): Promise<
     void
   > {
     this.timeout(timeoutMs)
 
     // GIVEN a Pay ID that will resolve on Testnet.
     const payIDClient = new XRPPayIDClient(XRPLNetwork.Test)
-    const payID = '$dev.payid.xpring.money/hbergren'
+    const payID = '$dev.payid.xpring.money/alice'
 
     // WHEN it is resolved to an XRP address on testnet
     const xrpAddress = await payIDClient.addressForPayID(payID)
@@ -40,11 +40,11 @@ describe('PayID Integration Tests', function(): void {
     assert.equal(xrpAddress, 'TVacixsWrqyWCr98eTYP7FSzE9NwupESR4TrnijN7fccNiS')
   })
 
-  it('Resolve PayID to XRP - unknown PayID - devnet', function(done) {
+  it('Resolve PayID to XRP - unknown PayID - devnet', function (done) {
     this.timeout(timeoutMs)
 
     // GIVEN a Pay ID that will not resolve on Devnet.
-    const payID = '$dev.payid.xpring.money/hbergren'
+    const payID = '$dev.payid.xpring.money/alice'
     const network = XRPLNetwork.Dev
     const payIDClient = new XRPPayIDClient(network)
 
@@ -64,7 +64,7 @@ describe('PayID Integration Tests', function(): void {
     })
   })
 
-  it('getInvoice', async function(): Promise<void> {
+  it('getInvoice', async function (): Promise<void> {
     this.timeout(timeoutMs)
 
     // GIVEN a Pay ID.
@@ -78,7 +78,7 @@ describe('PayID Integration Tests', function(): void {
     assert.exists(invoice)
   })
 
-  it('postInvoice', async function(): Promise<void> {
+  it('postInvoice', async function (): Promise<void> {
     this.timeout(timeoutMs)
 
     // GIVEN a Pay ID.
@@ -107,7 +107,7 @@ describe('PayID Integration Tests', function(): void {
     assert.exists(invoice)
   })
 
-  it('receipt', async function(): Promise<void> {
+  it('receipt', async function (): Promise<void> {
     // GIVEN a Pay ID.
     const payID = '$travel.payid.xpring.money/dino'
     const payIDClient = new XRPPayIDClient(XRPLNetwork.Main)
