2c71b8bfd6da1c22374b2715f5a0981d42ba4b99
Ted Kalaw <ted.kalaw@gmail.com>
2020-04-22 16:32:41 -0700

    Adds space-before-function-paren

--- test/XRP/rippled-flags-test.ts
+++ test/XRP/rippled-flags-test.ts
@@ -3,8 +3,8 @@ import { assert } from 'chai'
 import RippledFlags from '../../src/XRP/rippled-flags'
 import 'mocha'
 
-describe('rippled flags', function(): void {
-  it('check - flag present', function(): void {
+describe('rippled flags', function (): void {
+  it('check - flag present', function (): void {
     // GIVEN a set of flags that contains the tfPartialPayment flag.
     const flags = RippledFlags.TF_PARTIAL_PAYMENT | 1 | 4 // 1 and 4 are arbitrarily chosen numbers.
 
@@ -14,7 +14,7 @@ describe('rippled flags', function(): void {
     )
   })
 
-  it('check - flag notpresent', function(): void {
+  it('check - flag notpresent', function (): void {
     // GIVEN a set of flags that does not contain the tfPartialPayment flag.
     const flags = 1 | 4 // 1 and 4 are arbitrarily chosen numbers.
 
