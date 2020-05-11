f8f14fc869db9d96363b63e1493dbcaa0a4d8c3e
Keefer Taylor <keefer.t@gmail.com>
2020-05-11 22:52:14 +0900

    Update xrp-utils-test.ts

--- test/XRP/xrp-utils-test.ts
+++ test/XRP/xrp-utils-test.ts
@@ -1,1 +1,34 @@
-// This file left blank to prove code coverage checks work. 
+import { assert } from 'chai'	
+import BigNumber from 'bignumber.js'	
+import { dropsToXrp, xrpToDrops } from '../../src/XRP/xrp-utils'	
+import 'mocha'	
+
+describe('xrp-drops-conversion', function (): void {	
+  // xrpToDrops and dropsToXrp tests	
+  it('dropsToXrp() - works with a typical amount', function (): void {	
+    // GIVEN a typical, valid drops value, WHEN converted to xrp	
+    const xrp = dropsToXrp('2000000')	
+
+    // THEN the conversion is as expected	
+    assert.strictEqual(xrp, '2', '2 million drops equals 2 XRP')	
+  })	
+
+  it('dropsToXrp() - works with fractions', function (): void {	
+    // GIVEN drops amounts that convert to fractional xrp amounts	
+    // WHEN converted to xrp THEN the conversion is as expected	
+    let xrp = dropsToXrp('3456789')	
+    assert.strictEqual(xrp, '3.456789', '3,456,789 drops equals 3.456789 XRP')	
+
+    xrp = dropsToXrp('3400000')	
+    assert.strictEqual(xrp, '3.4', '3,400,000 drops equals 3.4 XRP')	
+
+    xrp = dropsToXrp('1')	
+    assert.strictEqual(xrp, '0.000001', '1 drop equals 0.000001 XRP')	
+
+    xrp = dropsToXrp('1.0')	
+    assert.strictEqual(xrp, '0.000001', '1.0 drops equals 0.000001 XRP')	
+
+    xrp = dropsToXrp('1.00')	
+    assert.strictEqual(xrp, '0.000001', '1.00 drops equals 0.000001 XRP')	
+  })
+})
