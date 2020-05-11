1eedb1a2378c3be49306b60a7f75491ed0939a46
Keefer Taylor <keefer.t@gmail.com>
2020-05-12 00:28:04 +0900

    prune tx blob

--- test/Common/utils-test.ts
+++ test/Common/utils-test.ts
@@ -273,33 +273,33 @@ describe('utils', function (): void {
     assert.isFalse(isValid)
   })
 
-  it('transactionBlobHex() - Valid transaction blob', function (): void {
-    // GIVEN a transaction blob.
-    const transactionBlobHex =
-      '120000240000000561400000000000000168400000000000000C73210261BBB9D242440BA38375DAD79B146E559A9DFB99055F7077DA63AE0D643CA0E174473045022100C8BB1CE19DFB1E57CDD60947C5D7F1ACD10851B0F066C28DBAA3592475BC3808022056EEB85CC8CD41F1F1CF635C244943AD43E3CF0CE1E3B7359354AC8A62CF3F488114F8942487EDB0E4FD86190BF8DCB3AF36F608839D83141D10E382F805CD7033CC4582D2458922F0D0ACA6'
+  // it('transactionBlobHex() - Valid transaction blob', function (): void {
+  //   // GIVEN a transaction blob.
+  //   const transactionBlobHex =
+  //     '120000240000000561400000000000000168400000000000000C73210261BBB9D242440BA38375DAD79B146E559A9DFB99055F7077DA63AE0D643CA0E174473045022100C8BB1CE19DFB1E57CDD60947C5D7F1ACD10851B0F066C28DBAA3592475BC3808022056EEB85CC8CD41F1F1CF635C244943AD43E3CF0CE1E3B7359354AC8A62CF3F488114F8942487EDB0E4FD86190BF8DCB3AF36F608839D83141D10E382F805CD7033CC4582D2458922F0D0ACA6'
 
-    // WHEN the transaction blob is converted to a hash.
-    const transactionHash = Utils.transactionBlobToTransactionHash(
-      transactionBlobHex,
-    )
+  //   // WHEN the transaction blob is converted to a hash.
+  //   const transactionHash = Utils.transactionBlobToTransactionHash(
+  //     transactionBlobHex,
+  //   )
 
-    // THEN the transaction blob is as expected.
-    assert.strictEqual(
-      transactionHash,
-      '7B9F6E019C2A79857427B4EF968D77D683AC84F5A880830955D7BDF47F120667',
-    )
-  })
+  //   // THEN the transaction blob is as expected.
+  //   assert.strictEqual(
+  //     transactionHash,
+  //     '7B9F6E019C2A79857427B4EF968D77D683AC84F5A880830955D7BDF47F120667',
+  //   )
+  // })
 
-  it('transactionBlobHex() - Invalid transaction blob', function (): void {
-    // GIVEN an invalid transaction blob.
-    const transactionBlobHex = 'xrp'
+  // it('transactionBlobHex() - Invalid transaction blob', function (): void {
+  //   // GIVEN an invalid transaction blob.
+  //   const transactionBlobHex = 'xrp'
 
-    // WHEN the transaction blob is converted to a hash.
-    const transactionHash = Utils.transactionBlobToTransactionHash(
-      transactionBlobHex,
-    )
+  //   // WHEN the transaction blob is converted to a hash.
+  //   const transactionHash = Utils.transactionBlobToTransactionHash(
+  //     transactionBlobHex,
+  //   )
 
-    // THEN the hash is undefined.
-    assert.isUndefined(transactionHash)
-  })
+  //   // THEN the hash is undefined.
+  //   assert.isUndefined(transactionHash)
+  // })
 })
