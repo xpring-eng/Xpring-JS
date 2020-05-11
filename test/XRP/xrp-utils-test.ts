6f4fa40e972dcea9b0d45c1a00aefa0dc3896f77
Keefer Taylor <keefer.t@gmail.com>
2020-05-12 00:04:51 +0900

    more pruning

--- test/XRP/xrp-utils-test.ts
+++ test/XRP/xrp-utils-test.ts
@@ -4,25 +4,6 @@ import { dropsToXrp, xrpToDrops } from '../../src/XRP/xrp-utils'
 import 'mocha'
 
 describe('xrp-drops-conversion', function (): void {
-  it('dropsToXrp() - works with fractions', function (): void {
-    // GIVEN drops amounts that convert to fractional xrp amounts
-    // WHEN converted to xrp THEN the conversion is as expected
-    let xrp = dropsToXrp('3456789')
-    assert.strictEqual(xrp, '3.456789', '3,456,789 drops equals 3.456789 XRP')
-
-    xrp = dropsToXrp('3400000')
-    assert.strictEqual(xrp, '3.4', '3,400,000 drops equals 3.4 XRP')
-
-    xrp = dropsToXrp('1')
-    assert.strictEqual(xrp, '0.000001', '1 drop equals 0.000001 XRP')
-
-    xrp = dropsToXrp('1.0')
-    assert.strictEqual(xrp, '0.000001', '1.0 drops equals 0.000001 XRP')
-
-    xrp = dropsToXrp('1.00')
-    assert.strictEqual(xrp, '0.000001', '1.00 drops equals 0.000001 XRP')
-  })
-
   it('dropsToXrp() - works with zero', function (): void {
     // GIVEN several equivalent representations of zero
     // WHEN converted to xrp, THEN the result is zero
