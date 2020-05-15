6c96cb2c8cfae833cbfa355fa7b0ad03f79f1d89
Keefer Taylor <keefer.t@gmail.com>
2020-05-16 03:51:22 +0900

    fix reliable submission bug

--- src/XRP/xrp-client-decorator.ts
+++ src/XRP/xrp-client-decorator.ts
@@ -43,9 +43,19 @@ export interface XRPClientDecorator {
   /**
    * Retrieve the latest validated ledger sequence on the XRP Ledger.
    *
-   * @returns The index of the latest validated ledger.
+   * Note: This call will throw if the given account does not exist on the ledger at the current time. It is the
+   * *caller's responsibility* to ensure this invariant is met.
+   *
+   * Note: The input address *must* be in a classic address form. Inputs are not checked to this internal method.
+   *
+   * TODO(keefertaylor): The above requirements are onerous, difficult to reason about and the logic of this method is
+   * brittle. Replace this method's implementation when rippled supports a `ledger` RPC via gRPC.
+   *
+   * @param address An address that exists at the current time. The address is unchecked and must be a classic address.
+   * @return The index of the latest validated ledger.
+   * @throws XRPException If there was a problem communicating with the XRP Ledger.
    */
-  getLastValidatedLedgerSequence(): Promise<number>
+  getLatestValidatedLedgerSequence(address: string): Promise<number>
 
   /**
    * Retrieve the raw transaction status for the given transaction hash.
