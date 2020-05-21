613ad648274572952474c19699c73b102032c9c3
Amie Corso <acorso@ripple.com>
2020-05-21 08:14:10 -0700

    namespace XRPUtils

--- src/XRP/index.ts
+++ src/XRP/index.ts
@@ -1,6 +1,7 @@
 export { default as RippledFlags } from './rippled-flags'
 export { default as TransactionStatus } from './transaction-status'
 export { default as XRPClient } from './xrp-client'
+export { default as XRPUtils } from './xrp-utils'
 export {
   XRPCurrencyAmount,
   XRPCurrency,
