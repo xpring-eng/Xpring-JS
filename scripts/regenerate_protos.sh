de5cd0ab3381d990c531e22e40a2d639b33faa42
Tyler Longwell <tlongwell@ripple.com>
2020-05-27 13:56:02 -0400

    workwork

--- scripts/regenerate_protos.sh
+++ scripts/regenerate_protos.sh
@@ -19,14 +19,14 @@ mkdir -p $XRP_OUT_DIR_WEB
 mkdir -p $XRP_OUT_DIR_NODE
 
 # Generate web code.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --js_out=import_style=commonjs,binary:$XRP_OUT_DIR_WEB \
     --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:$XRP_OUT_DIR_WEB \
     --proto_path $PROTO_PATH \
     $PROTO_SRC_FILES
 
 # Generate node code.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --js_out=import_style=commonjs,binary:$XRP_OUT_DIR_NODE \
     --grpc_out=$XRP_OUT_DIR_NODE \
     --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
@@ -34,7 +34,7 @@ $PWD/node_modules/grpc-tools/bin/protoc \
     $PROTO_SRC_FILES
 
 # Generate node typescript declaration files.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=$XRP_OUT_DIR_NODE \
     --proto_path=$PROTO_PATH \
@@ -54,26 +54,26 @@ mkdir -p $ILP_OUT_DIR_WEB
 mkdir -p $ILP_OUT_DIR_NODE
 
 # Generate web code.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --js_out=import_style=commonjs,binary:$ILP_OUT_DIR_WEB \
     --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:$ILP_OUT_DIR_WEB \
-    --proto_path=$PWD/hermes-ilp/protocol-buffers/proto \
-    $PWD/hermes-ilp/protocol-buffers/proto/*.proto
+    --proto_path=./hermes-ilp/protocol-buffers/proto \
+    ./hermes-ilp/protocol-buffers/proto/*.proto
 
 # Generate node code.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --js_out=import_style=commonjs,binary:$ILP_OUT_DIR_NODE \
     --grpc_out=$ILP_OUT_DIR_NODE \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-    --proto_path=$PWD/hermes-ilp/protocol-buffers/proto \
-    $PWD/hermes-ilp/protocol-buffers/proto/*.proto
+    --proto_path=./hermes-ilp/protocol-buffers/proto \
+    ./hermes-ilp/protocol-buffers/proto/*.proto
 
 # Generate node typescript declaration files.
-$PWD/node_modules/grpc-tools/bin/protoc \
+./node_modules/grpc-tools/bin/protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
     --ts_out=$ILP_OUT_DIR_NODE \
-    --proto_path=$PWD/hermes-ilp/protocol-buffers/proto \
-    $PWD/hermes-ilp/protocol-buffers/proto/*.proto
+    --proto_path=./hermes-ilp/protocol-buffers/proto \
+    ./hermes-ilp/protocol-buffers/proto/*.proto
 
 echo "All done!"
