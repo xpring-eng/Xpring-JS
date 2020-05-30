211c2311b5fae4ad01c2f3cafda6dede31c53d2e
Amie Corso <acorso@ripple.com>
2020-05-30 16:02:30 -0700

    fakes and tests

--- test/XRP/fakes/fake-xrp-transaction-type-protobufs.ts
+++ test/XRP/fakes/fake-xrp-transaction-type-protobufs.ts
@@ -1,6 +1,7 @@
 import {
   AccountSet,
   AccountDelete,
+  CheckCancel,
 } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/transaction_pb'
 import {
   ClearFlag,
@@ -12,12 +13,13 @@ import {
   TickSize,
   Destination,
   DestinationTag,
+  CheckID,
 } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/common_pb'
 import { AccountAddress } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/account_pb'
 
 // Primitive test values ===============================================================
 
-// AccountSet
+// AccountSet values
 const testClearFlag = 5
 const testDomain = 'testdomain'
 const testEmailHash = new Uint8Array([8, 9, 10])
@@ -26,10 +28,13 @@ const testSetFlag = 4
 const testTransferRate = 1234567890
 const testTickSize = 7
 
-// AccountDelete
+// AccountDelete values
 const testDestination = 'rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY'
 const testDestinationTag = 13
 
+// CheckCancel values
+const testCheckId =
+  '49647F0D748DC3FE26BDACBC57F251AADEFFF391403EC9BF87C97F67E9977FB0'
 // Protobuf objects ======================================================================
 
 // AccountSet protos
@@ -83,9 +88,17 @@ testAccountDeleteProto.setDestinationTag(testDestinationTagProto)
 const testAccountDeleteProtoNoTag = new AccountDelete()
 testAccountDeleteProtoNoTag.setDestination(testDestinationProto)
 
+// CheckCancel proto
+const testCheckIdProto = new CheckID()
+testCheckIdProto.setValue(testCheckId)
+
+const testCheckCancelProto = new CheckCancel()
+testCheckCancelProto.setCheckId(testCheckIdProto)
+
 export {
   testAccountSetProtoAllFields,
   testAccountSetProtoOneFieldSet,
   testAccountDeleteProto,
   testAccountDeleteProtoNoTag,
+  testCheckCancelProto,
 }
