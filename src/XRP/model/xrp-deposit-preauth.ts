02e09704d4498118d6e2fcf2396fd3297621680f
Amie Corso <acorso@ripple.com>
2020-05-30 19:38:48 -0700

    initial impl XRPDepositAuth

--- src/XRP/model/xrp-deposit-preauth.ts
+++ src/XRP/model/xrp-deposit-preauth.ts
@@ -1,10 +1,11 @@
 import { DepositPreauth } from '../Generated/web/org/xrpl/rpc/v1/transaction_pb'
 
-
 /*
  * Represents a DepositPreauth transaction on the XRP Ledger.
+ * A DepositPreauth transaction gives another account pre-approval to deliver payments to the sender of this transaction.
+ * This is only useful if the sender of this transaction is using (or plans to use) Deposit Authorization.
  *
- * @see:
+ * @see: https://xrpl.org/depositpreauth.html
  */
 export default class XRPDepositPreauth {
   /**
@@ -12,16 +13,26 @@ export default class XRPDepositPreauth {
    *
    * @param depositPreauth a DepositPreauth (protobuf object) whose field values will be used to construct an XRPDepositPreauth
    * @return an XRPDepositPreauth with its fields set via the analogous protobuf fields.
-   * @see
+   * @see https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L159
    */
-  public static from(depositPreauth: DepositPreauth): XRPDepositPreauth | undefined {
-)
-    return new XRPDepositPreauth(
-,
-    )
+  public static from(
+    depositPreauth: DepositPreauth,
+  ): XRPDepositPreauth | undefined {
+    const authorize = depositPreauth.getAuthorize()?.getValue()?.getAddress()
+    const unauthorize = depositPreauth
+      .getUnauthorize()
+      ?.getValue()
+      ?.getAddress()
+    return new XRPDepositPreauth(authorize, unauthorize)
   }
 
+  /**
+   *
+   * @param authorize (Optional) The XRP Ledger address of the sender to preauthorize.
+   * @param unauthorize (Optional) The XRP Ledger address of a sender whose preauthorization should be revoked.
+   */
   private constructor(
-
+    readonly authorize?: string,
+    readonly unauthorize?: string,
   ) {}
 }
