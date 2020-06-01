a97e52656af2020b22b03fc5ba7ccdd5d7168e96
Ian Simpson <ian.g.simpson@gmail.com>
2020-06-01 13:39:48 -0700

    document util methods
    Signed-off-by: Ian Simpson <ian.g.simpson@gmail.com>

--- src/Common/utils.ts
+++ src/Common/utils.ts
@@ -4,6 +4,12 @@ const isNode = (): boolean => {
   return process?.release?.name === 'node'
 }
 
+/**
+ * Converts a string such that each of its characters are represented as hex.
+ *
+ * @param value - the string to convert to hex.
+ * @returns the hex encoded version of the string.
+ */
 export const stringToHex = (value: string): string => {
   let result = ''
   // eslint-disable-next-line no-plusplus
@@ -13,6 +19,13 @@ export const stringToHex = (value: string): string => {
   return result
 }
 
+/**
+ * Converts a string that is optionally in hex format into a Uint8Array.
+ *
+ * @param value - the string to convert to a Uint8Array.
+ * @param isHex - flag to indicate it's a hex string or not.
+ * @returns the Uint8Array value.
+ */
 export const stringToUint8Array = (
   value?: string,
   isHex?: boolean,
