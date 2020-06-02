b2fb2c56ed68a3799e1aa0acc42b86255ff81f01
Amie Corso <acorso@ripple.com>
2020-06-02 13:56:51 -0700

    specify testnet in fake proto/model objects

--- test/XRP/fakes/fake-xrp-protobufs.ts
+++ test/XRP/fakes/fake-xrp-protobufs.ts
@@ -37,6 +37,7 @@ import { GetAccountTransactionHistoryResponse } from '../../../src/XRP/Generated
 import XRPTransaction from '../../../src/XRP/model/xrp-transaction'
 import { GetTransactionResponse } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/get_transaction_pb'
 import { Meta } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/meta_pb'
+import XRPLNetwork from '../../../src/Common/xrpl-network'
 
 // primitive test values
 const testCurrencyName = 'currencyName'
@@ -407,6 +408,7 @@ testInvalidGetAccountTransactionHistoryResponse.setTransactionsList(
 // test XRPTransaction
 const testXRPTransaction = XRPTransaction.from(
   testGetTransactionResponseProto,
+  XRPLNetwork.Test,
 )!!
 
 export {
