169780da93ecb3450a326ce364708df84563785b
Amie Corso <acorso@ripple.com>
2020-06-03 14:06:28 -0700

    impl XRPOfferCancel

---
+++ src/XRP/model/xrp-offer-cancel.ts
@@ -0,0 +1,29 @@
+import { OfferCancel } from '../Generated/web/org/xrpl/rpc/v1/transaction_pb'
+
+/*
+ * Represents an OfferCancel transaction on the XRP Ledger.
+ * An OfferCancel transaction removes an Offer object from the XRP Ledger.
+ *
+ * @see: https://xrpl.org/offercancel.html
+ */
+export default class XRPOfferCancel {
+  /**
+   * Constructs an XRPOfferCancel from an OfferCancel protocol buffer.
+   *
+   * @param offerCancel an OfferCancel (protobuf object) whose field values will be used to construct an XRPOfferCancel
+   * @return an XRPOfferCancel with its fields set via the analogous protobuf fields.
+   * @see https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L206
+   */
+  public static from(offerCancel: OfferCancel): XRPOfferCancel | undefined {
+    const offerSequence = offerCancel.getOfferSequence()?.getValue()
+    return new XRPOfferCancel(offerSequence)
+  }
+
+  /**
+   *
+   * @param offerSequence The sequence number of a previous OfferCreate transaction. If specified,
+   *                      cancel any offer object in the ledger that was created by that transaction.
+   *                      It is not considered an error if the offer specified does not exist.
+   */
+  private constructor(readonly offerSequence?: number) {}
+}
