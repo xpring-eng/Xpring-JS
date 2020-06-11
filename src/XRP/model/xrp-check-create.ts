5d7ed96f1b09e8e133a7c86914d6b58727572f8d
Amie Corso <acorso@ripple.com>
2020-06-11 08:39:57 -0700

    xaddress

--- src/XRP/model/xrp-check-create.ts
+++ src/XRP/model/xrp-check-create.ts
@@ -1,5 +1,7 @@
+import { Utils } from 'xpring-common-js'
 import { CheckCreate } from '../Generated/web/org/xrpl/rpc/v1/transaction_pb'
 import { XRPCurrencyAmount } from '.'
+import XRPLNetwork from '../../Common/xrpl-network'
 
 /*
  * Represents a CheckCreate transaction on the XRP Ledger.
@@ -17,9 +19,22 @@ export default class XRPCheckCreate {
    * @return an XRPCheckCreate with its fields set via the analogous protobuf fields.
    * @see https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L145
    */
-  public static from(checkCreate: CheckCreate): XRPCheckCreate | undefined {
+  public static from(
+    checkCreate: CheckCreate,
+    xrplNetwork: XRPLNetwork,
+  ): XRPCheckCreate | undefined {
     const destination = checkCreate.getDestination()?.getValue()?.getAddress()
     if (!destination) {
+      return undefined
+    }
+    const destinationTag = checkCreate.getDestinationTag()?.getValue()
+
+    const destinationXAddress = Utils.encodeXAddress(
+      destination,
+      destinationTag,
+      xrplNetwork == XRPLNetwork.Test || xrplNetwork == XRPLNetwork.Dev,
+    )
+    if (!destinationXAddress) {
       return undefined
     }
 
@@ -31,31 +46,28 @@ export default class XRPCheckCreate {
     if (!sendMax) {
       return undefined
     }
-    const destinationTag = checkCreate.getDestinationTag()?.getValue()
     const expiration = checkCreate.getExpiration()?.getValue()
     const invoiceId = checkCreate.getInvoiceId()?.getValue_asB64()
     return new XRPCheckCreate(
-      destination,
+      destinationXAddress,
       sendMax,
-      destinationTag,
       expiration,
       invoiceId,
     )
   }
 
   /**
-   * @param destination The unique address of the account that can cash the Check.
+   * @param destinationXAddress The unique address and (optional) destination tag of the account that can cash the Check,
+   *                            encoded as an X-address (see https://xrpaddress.info/).
    * @param sendMax Maximum amount of source currency the Check is allowed to debit the sender, including transfer fees on non-XRP currencies.
    *                The Check can only credit the destination with the same currency (from the same issuer, for non-XRP currencies).
    *                For non-XRP amounts, the nested field names MUST be lower-case.
-   * @param destinationTag (Optional) Arbitrary tag that identifies the reason for the Check, or a hosted recipient to pay.
    * @param expiration (Optional) Time after which the Check is no longer valid, in seconds since the Ripple Epoch.
    * @param invoiceId (Optional) Arbitrary 256-bit hash representing a specific reason or identifier for this Check.
    */
   private constructor(
-    readonly destination: string,
+    readonly destinationXAddress: string,
     readonly sendMax: XRPCurrencyAmount,
-    readonly destinationTag?: number,
     readonly expiration?: number,
     readonly invoiceId?: string,
   ) {}
