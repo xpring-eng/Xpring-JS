56614e28a22ae639dd15034fdbdbf469fdd67598
Amie Corso <acorso@ripple.com>
2020-06-11 10:00:47 -0700

    xaddr in class

--- src/XRP/model/xrp-escrow-create.ts
+++ src/XRP/model/xrp-escrow-create.ts
@@ -1,5 +1,7 @@
+import { Utils } from 'xpring-common-js'
 import { EscrowCreate } from '../Generated/web/org/xrpl/rpc/v1/transaction_pb'
 import { XRPCurrencyAmount } from '.'
+import XRPLNetwork from '../../Common/xrpl-network'
 
 /*
  * Represents an EscrowCreate transaction on the XRP Ledger.
@@ -16,7 +18,10 @@ export default class XrpEscrowCreate {
    * @return an XrpEscrowCreate with its fields set via the analogous protobuf fields.
    * @see https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L178
    */
-  public static from(escrowCreate: EscrowCreate): XrpEscrowCreate | undefined {
+  public static from(
+    escrowCreate: EscrowCreate,
+    xrplNetwork: XRPLNetwork,
+  ): XrpEscrowCreate | undefined {
     // amount is a required field
     const amountCurrencyAmountProto = escrowCreate.getAmount()?.getValue()
     if (!amountCurrencyAmountProto) {
@@ -26,24 +31,32 @@ export default class XrpEscrowCreate {
     if (!amount) {
       return undefined
     }
-    // destination is a required field
+
     const destination = escrowCreate.getDestination()?.getValue()?.getAddress()
     if (!destination) {
+      return undefined
+    }
+    const destinationTag = escrowCreate.getDestinationTag()?.getValue()
+    const destinationXAddress = Utils.encodeXAddress(
+      destination,
+      destinationTag,
+      xrplNetwork == XRPLNetwork.Test || xrplNetwork == XRPLNetwork.Dev,
+    )
+    // destinationXAddress is a required field
+    if (!destinationXAddress) {
       return undefined
     }
 
     const cancelAfter = escrowCreate.getCancelAfter()?.getValue()
     const finishAfter = escrowCreate.getFinishAfter()?.getValue()
     const condition = escrowCreate.getCondition()?.getValue_asB64()
-    const destinationTag = escrowCreate.getDestinationTag()?.getValue()
 
     return new XrpEscrowCreate(
       amount,
-      destination,
+      destinationXAddress,
       cancelAfter,
       finishAfter,
       condition,
-      destinationTag,
     )
   }
 
@@ -51,22 +64,20 @@ export default class XrpEscrowCreate {
    * @param amount Amount of XRP, in drops, to deduct from the sender's balance and escrow.
    *                Once escrowed, the XRP can either go to the Destination address (after the FinishAfter time)
    *                or returned to the sender (after the CancelAfter time).
-   * @param destination Address to receive escrowed XRP.
+   * @param destinationXAddress Address and (optional) destination tag to receive escrowed XRP, encoded as an X-address.
+   *                            (See https://xrpaddress.info/)
    * @param cancelAfter (Optional) The time, in seconds since the Ripple Epoch, when this escrow expires.
    *                    This value is immutable; the funds can only be returned the sender after this time.
    * @param finishAfter (Optional) The time, in seconds since the Ripple Epoch, when the escrowed XRP can be released to the recipient.
    *                    This value is immutable; the funds cannot move until this time is reached.
    * @param condition (Optional) Hex value representing a PREIMAGE-SHA-256 crypto-condition.
    *                   The funds can only be delivered to the recipient if this condition is fulfilled.
-   * @param destinationTag (Optional) Arbitrary tag to further specify the destination for this escrowed payment,
-   *                        such as a hosted recipient at the destination address.
    */
   private constructor(
-    readonly amount?: XRPCurrencyAmount,
-    readonly destination?: string,
+    readonly amount: XRPCurrencyAmount,
+    readonly destinationXAddress: string,
     readonly cancelAfter?: number,
     readonly finishAfter?: number,
     readonly condition?: string,
-    readonly destinationTag?: number,
   ) {}
 }
