c446a837c2468224c8e7396b159d3d449108f727
Amie Corso <acorso@ripple.com>
2020-06-23 16:15:57 -0700

    update payid fakes and tests

--- test/PayID/pay-id-client.test.ts
+++ test/PayID/pay-id-client.test.ts
@@ -12,7 +12,7 @@ describe('PayIdClient', function (): void {
   })
 
   it('xrpAddressForPayId - invalid Pay ID', function (done): void {
-    // GIVEN a PayIDClient and an invalid PayID.
+    // GIVEN a PayIdClient and an invalid PayID.
     const invalidPayID = 'xpring.money/georgewashington' // Does not start with '$'
     const payIDClient = new PayIdClient(XrplNetwork.Test)
 
@@ -54,7 +54,7 @@ describe('PayIdClient', function (): void {
   })
 
   it('xrpAddressForPayId - successful response - match not found', function (done) {
-    // GIVEN a PayIDClient, valid PayID and mocked networking to return a 404 for the payID.
+    // GIVEN a PayIdClient, valid PayID and mocked networking to return a 404 for the payID.
     const payId = 'georgewashington$xpring.money'
     const payIdClient = new PayIdClient(XrplNetwork.Test)
     const network = XrplNetwork.Test
@@ -82,7 +82,7 @@ describe('PayIdClient', function (): void {
   })
 
   it('xrpAddressForPayId - unknown mime type', function (done) {
-    // GIVEN a PayIDClient and with mocked networking to return a server error.
+    // GIVEN a PayIdClient and with mocked networking to return a server error.
     const payId = 'georgewashington$xpring.money'
     const payIdClient = new PayIdClient(XrplNetwork.Test)
 
