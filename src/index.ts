beb261ab0c7771b6c3b3b5d8f0ff38634472f1f2
Amie Corso <acorso@ripple.com>
2020-06-23 16:27:25 -0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,14 @@ export {
   Utils,
 } from 'xpring-common-js'
 export { PaymentRequest, PaymentResult, AccountBalance, IlpClient } from './ILP'
-export { default as PayIdClient, PayIDClient } from './PayID/pay-id-client'
-export {
-  PayIDErrorType,
-  PayIdErrorType,
-  PayIDError,
-  default as PayIdError,
-} from './PayID/pay-id-error'
+export { default as PayIdClient } from './PayID/pay-id-client'
+export { PayIdErrorType, default as PayIdError } from './PayID/pay-id-error'
 export { default as XrplNetwork, XRPLNetwork } from './Common/xrpl-network'
 export { default as ComplianceType } from './PayID/compliance-type'
 export { default as XpringClient } from './Xpring/xpring-client'
 export { default as XrpError, XRPErrorType } from './XRP/xrp-error'
 export { default as IlpError, IlpErrorType } from './ILP/ilp-error'
-export {
-  default as XrpPayIdClient,
-  XRPPayIDClient,
-} from './PayID/xrp-pay-id-client'
+export { default as XrpPayIdClient } from './PayID/xrp-pay-id-client'
 export { XRPUtils, XrpUtils } from './XRP'
 export { RippledFlags, TransactionStatus, XRPClient, XrpClient } from './XRP'
 export {
