26574f7a511d5557d28917077e879a589d330990
Amie Corso <acorso@ripple.com>
2020-07-16 18:33:05 -0700

    last RippledFlags refactor

--- test/XRP/rippled-flags.test.ts
+++ test/XRP/rippled-flags.test.ts
@@ -1,18 +1,18 @@
 /* eslint-disable no-bitwise -- Explicitly testing bitwise flags in this file */
 import { assert } from 'chai'
 
-import RippledFlags from '../../src/XRP/rippled-flags'
+import PaymentFlags from '../../src/XRP/model/payment-flags'
 import 'mocha'
 
 describe('rippled flags', function (): void {
   it('check - flag present', function (): void {
     // GIVEN a set of flags that contains the tfPartialPayment flag.
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- 1 and 4 are arbitrary chosen
-    const flags = RippledFlags.TF_PARTIAL_PAYMENT | 1 | 4
+    const flags = PaymentFlags.TF_PARTIAL_PAYMENT | 1 | 4
 
     // WHEN the presence of tfPartialPayment is checked THEN the flag is reported as present.
     assert.isTrue(
-      RippledFlags.checkFlag(RippledFlags.TF_PARTIAL_PAYMENT, flags),
+      PaymentFlags.checkFlag(PaymentFlags.TF_PARTIAL_PAYMENT, flags),
     )
   })
 
@@ -23,7 +23,7 @@ describe('rippled flags', function (): void {
 
     // WHEN the presence of tfPartialPayment is checked THEN the flag is reported as not present.
     assert.isFalse(
-      RippledFlags.checkFlag(RippledFlags.TF_PARTIAL_PAYMENT, flags),
+      PaymentFlags.checkFlag(PaymentFlags.TF_PARTIAL_PAYMENT, flags),
     )
   })
 })
