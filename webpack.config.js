9d539087a733662b30dbe336c251a4dbd52587a8
Ted Kalaw <ted.kalaw@gmail.com>
2020-07-31 14:32:59 -0700

    Adds sample webpack.config.js (#605)
    Copy of #480, didn't want to mess with conflicts
    
    npx webpack does not work with xpring-js out of the box because of some modules that require polyfilling, etc, particularly around grpc-js.

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+// webpack considers .web.ts and .ts to be different extensions
+// We use both for web vs node implementations of some clients.
+const moduleFileExtensions = ['js', 'web.ts', 'ts']
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/typescript', '@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: moduleFileExtensions.map((ext) => `.${ext}`),
+  },
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell webpack to provide empty mocks for them so importing them works.
+  node: {
+    module: 'empty',
+    dns: 'mock',
+    fs: 'empty',
+    http2: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'XpringJS',
+    libraryTarget: 'umd',
+    globalObject: "(typeof self !== 'undefined' ? self : this)",
+  },
+}
