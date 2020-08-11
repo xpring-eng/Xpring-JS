335b2755c5064dbe2ed678b255e42ddde7b92f5c
Amie Corso <acorso@ripple.com>
2020-08-11 09:15:25 -0700

    randomWalletFromFaucet

--- test/XRP/helpers/xrp-test-utils.ts
+++ test/XRP/helpers/xrp-test-utils.ts
@@ -1,12 +1,19 @@
 import { GetAccountTransactionHistoryResponse } from '../../../src/XRP/Generated/web/org/xrpl/rpc/v1/get_account_transaction_history_pb'
 import XrpTransaction from '../../../src/XRP/model/xrp-transaction'
-import { XrplNetwork } from 'xpring-common-js'
+import { Wallet, XrplNetwork } from 'xpring-common-js'
 import XrpMemo from '../../../src/XRP/model/xrp-memo'
+import bigInt from 'big-integer'
+import XrpClient from '../../../src/XRP/xrp-client'
+import axios from 'axios'
 
 /**
  * Convenience class for utility functions used in test cases for XrpClient infrastructure.
  */
 export default class XRPTestUtils {
+  static sleep = async (milliseconds: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  }
+
   /**
    * Converts a GetAccountTransactionHistoryResponse protocol buffer object into an array of XrpTransaction objects,
    * filtered only for PAYMENT type transactions.
@@ -28,6 +35,62 @@ export default class XRPTestUtils {
       }
     }
     return paymentXrpTransactions
+  }
+
+  /**
+   * Generates a random wallet and funds using the XRPL Testnet faucet.
+   */
+  static async randomWalletFromFaucet(): Promise<Wallet> {
+    const timeoutInSeconds = 20
+
+    const wallet = Wallet.generateRandomWallet().wallet
+    const address = wallet.getAddress()
+
+    const rippledUrl = 'test.xrp.xpring.io:50051'
+    const xrpClient = new XrpClient(rippledUrl, XrplNetwork.Test)
+
+    // Balance prior to asking for more funds
+    let startingBalance
+    try {
+      startingBalance = bigInt(await xrpClient.getBalance(address))
+    } catch {
+      startingBalance = bigInt('0')
+    }
+    // Ask the faucet to send funds to the given address
+    const faucetURL = 'https://faucet.altnet.rippletest.net/accounts'
+    await axios.post(faucetURL, { destination: address })
+
+    // Wait for the faucet to fund our account or until timeout
+    // Waits one second checks if balance has changed
+    // If balance doesn't change it will attempt again until timeoutInSeconds
+    for (
+      let balanceCheckCounter = 0;
+      balanceCheckCounter < timeoutInSeconds;
+      balanceCheckCounter++
+    ) {
+      // Wait 1 second
+      await this.sleep(1000)
+
+      // Request our current balance
+      let currentBalance
+      try {
+        currentBalance = bigInt(await xrpClient.getBalance(address))
+      } catch {
+        currentBalance = bigInt('0')
+      }
+      // If our current balance has changed then return
+      if (startingBalance.notEquals(currentBalance)) {
+        return wallet
+      }
+
+      // In the future if we had a tx hash from the faucet
+      // We should check the status of the tx which would be more accurate
+    }
+
+    // Balance did not update
+    throw new Error(
+      `Unable to fund address with faucet after waiting ${timeoutInSeconds} seconds`,
+    )
   }
 }
 
