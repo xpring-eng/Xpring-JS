41d91637748a8b971fbd506992322f0335966039
Ted Kalaw <ted.kalaw@gmail.com>
2020-09-22 17:00:53 -0700

    Improves codecov

--- test/XRP/default-xrp-client.test.ts
+++ test/XRP/default-xrp-client.test.ts
@@ -840,13 +840,12 @@ describe('Default XRP Client', function (): void {
       XrplNetwork.Test,
     )
 
-    const xAddressToAuthorize =
-      'X76YZJgkFzdSLZQTa7UzVSs34tFgyV2P16S3bvC8AWpmwdH'
+    const senderXAddress = 'X76YZJgkFzdSLZQTa7UzVSs34tFgyV2P16S3bvC8AWpmwdH'
     const { wallet } = Wallet.generateRandomWallet()!
 
     // WHEN authorizeDepositPreauth is called.
     const result = await xrpClient.authorizeDepositPreauth(
-      xAddressToAuthorize,
+      senderXAddress,
       wallet,
     )
     const transactionHash = result.hash
@@ -873,16 +872,30 @@ describe('Default XRP Client', function (): void {
       XrplNetwork.Test,
     )
 
-    const xAddressToAuthorize =
-      'X76YZJgkFzdSLZQTa7UzVSs34tFgyV2P16S3bvC8AWpmwdH'
+    const senderXAddress = 'X76YZJgkFzdSLZQTa7UzVSs34tFgyV2P16S3bvC8AWpmwdH'
     const { wallet } = Wallet.generateRandomWallet()!
 
     // WHEN authorizeDepositPreauth is attempted THEN an error is propagated.
-    xrpClient
-      .authorizeDepositPreauth(xAddressToAuthorize, wallet)
-      .catch((error) => {
-        assert.deepEqual(error, FakeXRPNetworkClientResponses.defaultError)
-        done()
-      })
+    xrpClient.authorizeDepositPreauth(senderXAddress, wallet).catch((error) => {
+      assert.deepEqual(error, FakeXRPNetworkClientResponses.defaultError)
+      done()
+    })
+  })
+
+  it('Authorize DepositPreauth - failure with malformed sender X-Address', function (done): void {
+    // GIVEN a DefaultXrpClient with mocked networking.
+    const xrpClient = new DefaultXrpClient(
+      fakeSucceedingNetworkClient,
+      XrplNetwork.Test,
+    )
+
+    const senderXAddress = 'notanxaddress'
+    const { wallet } = Wallet.generateRandomWallet()!
+
+    // WHEN authorizeDepositPreauth is attempted THEN an error is propagated.
+    xrpClient.authorizeDepositPreauth(senderXAddress, wallet).catch((error) => {
+      assert.deepEqual(error, XrpError.xAddressRequired)
+      done()
+    })
   })
 })
