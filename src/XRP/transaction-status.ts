835009b38653039e0df727c3383327bf56b734d1
Amie Corso <acorso@ripple.com>
2020-09-30 12:31:36 -0700

    new transaction status

--- src/XRP/transaction-status.ts
+++ src/XRP/transaction-status.ts
@@ -9,6 +9,9 @@ enum TransactionStatus {
   /** The transaction was included in a finalized ledger and succeeded. */
   Succeeded,
 
+  /** The transaction's last ledger sequence has been passed - the transaction cannot be included in a validated ledger. */
+  LastLedgerPassed,
+
   /** The transaction was included in a finalized ledger and succeeded. */
   Unknown,
 }
