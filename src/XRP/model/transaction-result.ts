6b2e3b11553b7206b5d2d274960961c569bf9546
Amie Corso <acorso@ripple.com>
2020-10-01 10:57:11 -0700

    add  field to TransactionStatus

--- src/XRP/model/transaction-result.ts
+++ src/XRP/model/transaction-result.ts
@@ -4,23 +4,17 @@ import TransactionStatus from '../transaction-status'
  * Represents the outcome of submitting an XRPL transaction.
  */
 export default class TransactionResult {
-  public readonly hash: string
-  public readonly status: TransactionStatus
-  public readonly validated: boolean
-
   /**
    * @param hash The identifying hash of the transaction.
-   * @param status The final result of the transaction.
+   * @param status The result of the transaction.
    * @param validated Whether this transaction (and status) are included in a validated ledger.
-   *                  The transaction status is only final if this field is true.
+   * @param final Whether this transaction result is the final outcome of the transaction on the XRPL.
+   *              The `status` and `validated` fields are subject to change unless this field is true.
    */
   public constructor(
-    hash: string,
-    status: TransactionStatus,
-    validated: boolean,
-  ) {
-    this.hash = hash
-    this.status = status
-    this.validated = validated
-  }
+    public readonly hash: string,
+    public readonly status: TransactionStatus,
+    public readonly validated: boolean,
+    public readonly final: boolean,
+  ) {}
 }
