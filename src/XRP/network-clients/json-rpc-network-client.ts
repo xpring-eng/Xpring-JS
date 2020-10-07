6dd62953376705877566ebfd552ab3dbca610e0b
Amie Corso <acorso@ripple.com>
2020-10-07 16:21:44 -0700

    initial impl of a network client

---
+++ src/XRP/network-clients/json-rpc-network-client.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+interface JsonRequest {
+  method: string
+  params: unknown
+}
+
+/**
+ * A network client for interacting with the rippled JSON RPC.
+ * @see https://xrpl.org/rippled-api.html
+ */
+export default class JsonRpcNetworkClient {
+  private readonly axiosInstance: AxiosInstance
+
+  public constructor(jsonUrl: string) {
+    this.axiosInstance = axios.create({
+      baseURL: jsonUrl,
+    })
+  }
+
+  public async submitRequest(
+    requestObject: JsonRequest,
+  ): Promise<AxiosResponse<unknown>> {
+    const requestOptions: AxiosRequestConfig = {
+      url: '/',
+      method: 'post',
+      data: requestObject,
+      headers: { 'Content-Type': 'application/json' },
+    }
+    return await this.axiosInstance.request(requestOptions)
+  }
+}
