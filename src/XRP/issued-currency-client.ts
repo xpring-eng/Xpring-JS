e93f6512df92c5a9970e8b73b1cabc7654592a13
Ted Kalaw <ted.kalaw@gmail.com>
2020-10-08 15:08:46 -0500

    Implements createTrustline

--- src/XRP/issued-currency-client.ts
+++ src/XRP/issued-currency-client.ts
@@ -1,9 +1,19 @@
-import { XrplNetwork } from 'xpring-common-js'
+import { Wallet, XrplNetwork } from 'xpring-common-js'
 import GrpcNetworkClient from './network-clients/grpc-xrp-network-client'
 import GrpcNetworkClientWeb from './network-clients/grpc-xrp-network-client.web'
 import { XrpNetworkClient } from './network-clients/xrp-network-client'
 import isNode from '../Common/utils'
 import CoreXrplClient from './core-xrpl-client'
+import { LimitAmount } from './Generated/web/org/xrpl/rpc/v1/common_pb'
+import XrpUtils from './shared/xrp-utils'
+import { TransactionResult, XrpError } from './shared'
+import { AccountAddress } from './Generated/web/org/xrpl/rpc/v1/account_pb'
+import {
+  Currency,
+  CurrencyAmount,
+  IssuedCurrencyAmount,
+} from './Generated/web/org/xrpl/rpc/v1/amount_pb'
+import { TrustSet } from './Generated/web/org/xrpl/rpc/v1/transaction_pb'
 
 /**
  * IssuedCurrencyClient is a client for working with Issued Currencies on the XRPL.
@@ -48,6 +58,59 @@ export default class IssuedCurrencyClient {
 
   public shutUpCompiler(): void {
     console.log(this.networkClient)
-    console.log(this.coreXrplClient)
+  }
+
+  /**
+   * Creates a trustline between this XRPL account and an issuer of an IssuedCurrency.
+   *
+   * @see https://xrpl.org/trustset.html
+   *
+   * TODO (tedkalaw): Implement qualityIn/qualityOut.
+   *
+   * @param issuerXAddress The X-Address of the issuer to extend trust to.
+   * @param currencyName The currency to this trust line applies to, as a three-letter ISO 4217 Currency Code  or a 160-bit hex value according to currency format.
+   * @param amount Decimal representation of the limit to set on this trust line.
+   * @param wallet The
+   */
+  public async createTrustline(
+    issuerXAddress: string,
+    currencyName: string,
+    amount: string,
+    wallet: Wallet,
+  ): Promise<TransactionResult> {
+    if (!XrpUtils.isValidXAddress(issuerXAddress)) {
+      throw XrpError.xAddressRequired
+    }
+
+    const issuerAccountAddress = new AccountAddress()
+    issuerAccountAddress.setAddress(issuerXAddress)
+
+    const currency = new Currency()
+    currency.setName(currencyName)
+
+    const issuedCurrencyAmount = new IssuedCurrencyAmount()
+    issuedCurrencyAmount.setCurrency(currency)
+    issuedCurrencyAmount.setIssuer(issuerAccountAddress)
+    // TODO (tedkalaw): Support other types of amounts.
+    issuedCurrencyAmount.setValue(amount)
+
+    const currencyAmount = new CurrencyAmount()
+    currencyAmount.setIssuedCurrencyAmount(issuedCurrencyAmount)
+
+    const limit = new LimitAmount()
+    limit.setValue(currencyAmount)
+
+    const trustSet = new TrustSet()
+    trustSet.setLimitAmount(limit)
+
+    const transaction = await this.coreXrplClient.prepareBaseTransaction(wallet)
+    transaction.setTrustSet(trustSet)
+
+    const transactionHash = await this.coreXrplClient.signAndSubmitTransaction(
+      transaction,
+      wallet,
+    )
+
+    return await this.coreXrplClient.getTransactionResult(transactionHash)
   }
 }
