e268f376e749f4b0f984c5f6c48919aae33de176
Mayukha Vadari <mvadari@gmail.com>
2020-10-14 09:59:15 -0700

    Implements `enableGlobalFreeze` on `IssuedCurrencyClient` (#803)
    * implement function
    
    * add unit tests
    
    * fix rebase issue
    
    * add integration tests
    
    * fix merge error

--- test/XRP/issued-currency-client.test.ts
+++ test/XRP/issued-currency-client.test.ts
@@ -456,4 +456,47 @@ describe('Issued Currency Client', function (): void {
         assert.deepEqual(error, FakeXRPNetworkClientResponses.defaultError)
       })
   })
+
+  it('enableGlobalFreeze - successful response', async function (): Promise<
+    void
+  > {
+    // GIVEN an IssuedCurrencyClient with mocked networking that will return a successful hash for submitTransaction
+    const issuedCurrencyClient = new IssuedCurrencyClient(
+      fakeSucceedingGrpcClient,
+      fakeSucceedingJsonClient,
+      XrplNetwork.Test,
+    )
+
+    // WHEN enableGlobalFreeze is called
+    const result = await issuedCurrencyClient.enableGlobalFreeze(this.wallet)
+    const transactionHash = result.hash
+
+    // THEN a transaction hash exists and is the expected hash
+    const expectedTransactionHash = Utils.toHex(
+      FakeXRPNetworkClientResponses.defaultSubmitTransactionResponse().getHash_asU8(),
+    )
+
+    assert.exists(transactionHash)
+    assert.strictEqual(transactionHash, expectedTransactionHash)
+  })
+
+  it('enableGlobalFreeze - submission failure', function (): void {
+    // GIVEN an IssuedCurrencyClient which will fail to submit a transaction.
+    const failureResponses = new FakeXRPNetworkClientResponses(
+      FakeXRPNetworkClientResponses.defaultAccountInfoResponse(),
+      FakeXRPNetworkClientResponses.defaultFeeResponse(),
+      FakeXRPNetworkClientResponses.defaultError,
+    )
+    const failingNetworkClient = new FakeXRPNetworkClient(failureResponses)
+    const issuedCurrencyClient = new IssuedCurrencyClient(
+      failingNetworkClient,
+      fakeSucceedingJsonClient,
+      XrplNetwork.Test,
+    )
+
+    // WHEN enableGlobalFreeze is attempted THEN an error is propagated.
+    issuedCurrencyClient.enableGlobalFreeze(this.wallet).catch((error) => {
+      assert.deepEqual(error, FakeXRPNetworkClientResponses.defaultError)
+    })
+  })
 })
