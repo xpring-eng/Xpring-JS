5fe8cd5b1d46901527088683a8b90ae44a2b7c91
Mayukha Vadari <mvadari@ripple.com>
2020-11-11 14:26:54 -0800

    include accidentally-removed param in doc

--- src/XRP/network-clients/web-socket-network-client-interface.ts
+++ src/XRP/network-clients/web-socket-network-client-interface.ts
@@ -14,6 +14,7 @@ export interface WebSocketNetworkClientInterface {
    * @see https://xrpl.org/monitor-incoming-payments-with-websocket.html
    *
    * @param account The account from which to subscribe to incoming transactions, encoded as a classic address.
+   * @param callback The function called whenever a new transaction is received.
    * @returns The response from the websocket confirming the subscription.
    */
   subscribeToAccount(
