fff4272b47345eec9f2448485376c57ac4afd180
Amie Corso <acorso@ripple.com>
2020-11-18 10:50:32 -0800

    make issued currency fields non-opt

--- src/XRP/network-clients/web-socket-network-client.ts
+++ src/XRP/network-clients/web-socket-network-client.ts
@@ -3,13 +3,8 @@ import { XrpError, XrpErrorType } from '../shared'
 import {
   WebSocketReadyState,
   RippledMethod,
-<<<<<<< HEAD
   WebSocketRequest,
   ResponseStatus,
-=======
-  ResponseStatus,
-  WebSocketRequest,
->>>>>>> ripple-path-find
   SubscribeRequest,
   AccountLinesRequest,
   GatewayBalancesRequest,
